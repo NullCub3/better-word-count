a19414642b5b40ab5c5b59940c897ebeccab30da
NullCube <nullcub3@gmail.com>
2024-07-20 07:39:16 -0700

    System Improvements

--- flake.nix
+++ flake.nix
@@ -5,17 +5,19 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux =
-      let
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in
-      {
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+          (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = with pkgs; [
             nodejs
           ];
         };
-      };
-  };
+      });
+    };
 }
